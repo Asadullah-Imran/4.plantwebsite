0e6ae2a7cd99f718f7caedea5772b81f79e22b6e
Asadullah-Imran <asadullahimran19@gmail.com>
2023-09-21 00:52:50 +0600

    before 30min

--- src/components/bodySection/Body.jsx
+++ src/components/bodySection/Body.jsx
@@ -1,6 +1,18 @@
 import "./Body.css";
+import Activity from "./activitySection/Activity";
+import Listing from "./listingSection/Listing";
+import Top from "./topSection/Top";
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div className="mainContent">
+      <Top />
+
+      <div className="bottom flex">
+        <Listing />
+        <Activity />
+      </div>
+    </div>
+  );
 };
 
 export default Body;
--- src/components/sidebarSection/Sidebar.jsx
+++ src/components/sidebarSection/Sidebar.jsx
@@ -1,5 +1,14 @@
-import "./Sidebar.css";
+import { AiOutlinePieChart } from "react-icons/ai";
+import { BiTrendingUp } from "react-icons/bi";
+import { BsCreditCard2Front, BsTrophy } from "react-icons/bs";
+import { IoMdSpeedometer } from "react-icons/io";
+import {
+  MdDeliveryDining,
+  MdOutlineExplore,
+  MdOutlinePermContactCalendar,
+} from "react-icons/md";
 
+import "./Sidebar.css";
 const Sidebar = () => {
   return (
     <div className="sideBar grid">
@@ -12,7 +21,62 @@ const Sidebar = () => {
         <ul className="menuLists grid">
           <li className="listItem">
             <a href="#" className="menuLink flex">
-              Icon
+              <IoMdSpeedometer className="icon" />
+              <span className="smallText">Dash Board</span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <MdDeliveryDining className="icon" />
+              <span className="smallText">My Orders</span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <MdOutlineExplore className="icon" />
+              <span className="smallText">Explore</span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <BsTrophy className="icon" />
+              <span className="smallText">Products</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="settingsDiv">
+        <h3 className="divTitle">SETTINGS</h3>
+        <ul className="menuLists grid">
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <AiOutlinePieChart className="icon" />
+              <span className="smallText">Charts</span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <BiTrendingUp className="icon" />
+              <span className="smallText">Trends</span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <MdOutlinePermContactCalendar className="icon" />
+              <span className="smallText">Contact</span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <BsCreditCard2Front className="icon" />
+              <span className="smallText">Billing</span>
             </a>
           </li>
         </ul>
